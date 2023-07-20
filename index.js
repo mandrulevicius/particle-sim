5bfa6f4d5443e3793e334f5cf49adc740e978b9d
Mindaugas Andrulevicius <mindaugas.andrulevicius@gmail.com>
2023-07-20 17:51:39 +0300

    particle system and future vision design

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 'use strict';
 
+const ATOM_PARTICLES = {
+  iron: ['Fe', 0.0003, 0.45]
+};
+const MOLECULE_PARTICLES = ['H2O'];
+const CELL_PARTICLES = [];
+const ORGAN_PARTICLES = [];
+const ORGANISM_PARTICLES = [];
+// maybe AI can do part of this?
+
 const activeParticles = [];
+const continuousInteractions = [];
+const passiveParticles = [];
 
 const lumpOfIron = {
-  mass: 3,
-  shc: 0.45,  // constant
-  currentEnergy: 27
+  template: ATOM_PARTICLES.iron,
+  numberOfParticles: 10000,
+  currentEnergyPerParticle: 0.0027,
+  position: { x: 30, y: -45, z: 0.4 }
 };
 
 const bucketOfWater = {
@@ -14,12 +26,16 @@ const bucketOfWater = {
   currentEnergy: 17
 };
 
-// add air, add sun
+// maybe should not only define position, but also relation to other objects. push or pull or merge.
+
+// add air(pocketOfParticles), add earth(lumpOfIron), add water (bucketOfWater), add sun
 
 const ZERO_ENERGY = 0;
 let time = 0;
 
 activeParticles.push(lumpOfIron, bucketOfWater);
+// add to active when new interactions.
+// if interaction constant and repeating, can remove from active interactions into continuous
 
 setInterval(() => {
   time += 1;
@@ -27,6 +43,9 @@ setInterval(() => {
   // for each active particle, check nearby energy, transfer.
   activeParticles.forEach(particle => {
     // TODO get close objects, transfer energy
+    // take a radius based on mass of the object
+    // or just touching, and everything else going to air?
+    // sun and other hot objects have different mechanism? or same really?
     const energyEmission = (particle.mass / particle.shc) * (particle.currentEnergy - ZERO_ENERGY);
     particle.currentEnergy -= energyEmission / 60;
     // per second rather than minute, depends on interval
@@ -34,3 +53,13 @@ setInterval(() => {
     console.log('energyEmission', energyEmission);
   });
 }, 1000);
+
+
+
+// after personal release, integrate with gta and make a demo of making a sword
+//  fully automated asset creation, with 3d models and textures included.
+
+// could throw the weapon (swing or thrust), a quickwheel action, checks how much the mouse was pulled,
+//  determines power of throw. speed determined by mass and power. air has push effect.
+
+// thrust weapon into wall, jump on the handle, jump up on wall picking up weapon and melee thrusting into wall higher up, then using that as a handle to climb.
